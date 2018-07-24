09ecaaec152824b36467e6132bf145422fde0057
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-07-24 14:44:47 +0100

    shift tests around

--- examples/src/test/scala/examples-test.scala
+++ examples/src/test/scala/examples-test.scala
@@ -2,12 +2,50 @@ import org.scalatest.FlatSpec
 
 class SetSpec extends FlatSpec {
 
- "A Poisson(10.0)" should "have mean 10.0" in {
-    import breeze.stats.distributions.Poisson
-    val p = Poisson(10.0)
-    val m = p.mean
-    assert(math.abs(m - 10.0) < 0.000001)
+
+  test("pfMll creation and evaluation") {
+    import LvPmmh._
+    import SpnExamples._
+    import Mll._
+    import scala.io.Source
+    val rawData = Source.fromFile("LVpreyNoise10.txt").getLines
+    val data = ((0 to 30 by 2).toList zip rawData.toList) map { x => (x._1.toDouble, DenseVector(x._2.toDouble)) }
+    val mll = pfMll(160, simPrior, 0.0, stepLv, obsLik, data)
+    val mlle = mll(lvparam)
+    // println(mlle)
+    assert(mlle < 0.0)
   }
+
+  test("Parallel pfMll creation and evaluation") {
+    import LvPmmh._
+    import SpnExamples._
+    import Mll._
+    import scala.io.Source
+    val rawData = Source.fromFile("LVpreyNoise10.txt").getLines
+    val data = ((0 to 30 by 2).toList zip rawData.toList) map { x => (x._1.toDouble, DenseVector(x._2.toDouble)) }
+    val mll = pfMllP(160, simPrior, 0.0, stepLv, obsLik, data)
+    val mlle = mll(lvparam)
+    // println(mlle)
+    assert(mlle < 0.0)
+  }
+
+  test("Serial and parallel pfMll should be similar") {
+    import LvPmmh._
+    import SpnExamples._
+    import Mll._
+    import scala.io.Source
+    val rawData = Source.fromFile("LVpreyNoise10.txt").getLines
+    val data = ((0 to 30 by 2).toList zip rawData.toList) map { x => (x._1.toDouble, DenseVector(x._2.toDouble)) }
+    val mll = pfMll(320, simPrior, 0.0, stepLv, obsLik, data)
+    val mllp = pfMllP(320, simPrior, 0.0, stepLv, obsLik, data)
+    val mlle = mll(lvparam)
+    val mllep = mllp(lvparam)
+    //println(mlle+" "+mllep)
+    assert(math.abs(mlle-mllep) < 2.0)
+  }
+
+
+
 
 }
 
--- src/test/scala/scala-smfsb-test.scala
+++ src/test/scala/scala-smfsb-test.scala
@@ -92,48 +92,6 @@ class MyTestSuite extends FunSuite {
     assert(ts.length === 1001)
   }
 
-  test("pfMll creation and evaluation") {
-    import LvPmmh._
-    import SpnExamples._
-    import Mll._
-    import scala.io.Source
-    val rawData = Source.fromFile("LVpreyNoise10.txt").getLines
-    val data = ((0 to 30 by 2).toList zip rawData.toList) map { x => (x._1.toDouble, DenseVector(x._2.toDouble)) }
-    val mll = pfMll(160, simPrior, 0.0, stepLv, obsLik, data)
-    val mlle = mll(lvparam)
-    // println(mlle)
-    assert(mlle < 0.0)
-  }
-
-  test("Parallel pfMll creation and evaluation") {
-    import LvPmmh._
-    import SpnExamples._
-    import Mll._
-    import scala.io.Source
-    val rawData = Source.fromFile("LVpreyNoise10.txt").getLines
-    val data = ((0 to 30 by 2).toList zip rawData.toList) map { x => (x._1.toDouble, DenseVector(x._2.toDouble)) }
-    val mll = pfMllP(160, simPrior, 0.0, stepLv, obsLik, data)
-    val mlle = mll(lvparam)
-    // println(mlle)
-    assert(mlle < 0.0)
-  }
-
-  test("Serial and parallel pfMll should be similar") {
-    import LvPmmh._
-    import SpnExamples._
-    import Mll._
-    import scala.io.Source
-    val rawData = Source.fromFile("LVpreyNoise10.txt").getLines
-    val data = ((0 to 30 by 2).toList zip rawData.toList) map { x => (x._1.toDouble, DenseVector(x._2.toDouble)) }
-    val mll = pfMll(320, simPrior, 0.0, stepLv, obsLik, data)
-    val mllp = pfMllP(320, simPrior, 0.0, stepLv, obsLik, data)
-    val mlle = mll(lvparam)
-    val mllep = mllp(lvparam)
-    //println(mlle+" "+mllep)
-    assert(math.abs(mlle-mllep) < 2.0)
-  }
-
-
 
 
 }
