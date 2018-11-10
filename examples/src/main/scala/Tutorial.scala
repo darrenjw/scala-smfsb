4da84f2fb70b5675ca05649b18ee75c0caa4c245
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-11-10 21:53:49 +0000

    starting on tutorial examples

---
+++ examples/src/main/scala/Tutorial.scala
@@ -0,0 +1,40 @@
+/*
+Tutorial.scala
+
+Stuff for the tutorial walk-through
+
+ */
+
+object Tutorial {
+
+  def main(args: Array[String]): Unit = {
+
+// ***************************************************************
+    println("Start")
+
+    import smfsb._
+    import smfsb.Types._
+    import breeze.linalg._
+    import breeze.numerics._
+
+    // Simulate LV with Gillespie
+
+    // Simulate LV with non-default parameters
+
+    // Simulate other models with Gillespie
+
+    // Other simulation functions
+
+    // Simulate LV with other algorithms
+
+    // PMCMC based parameter inference in other example
+
+    println("End")
+// ***************************************************************
+
+  }
+
+}
+
+// eof
+
