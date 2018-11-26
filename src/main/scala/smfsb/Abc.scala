25c90503a86063e7fe2b4d066aea229a690e675b
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-11-26 13:52:03 +0000

    Scaladoc update

--- src/main/scala/smfsb/Abc.scala
+++ src/main/scala/smfsb/Abc.scala
@@ -16,7 +16,21 @@ object Abc {
 
   import scala.collection.GenSeq
 
-  // TODO: scaladoc
+  /**
+    * Function for running ABC simulations in parallel on all cores. This
+    * function simply carries out the runs. No post-processing (including
+    * rejection) is carried out.
+    * 
+    * @param n The number of ABC simulations to be carried out
+    * @param rprior Function returing a single simulated parameter value 
+    * from a prior
+    * @param dist Function that takes a single parameter value and runs a
+    * forward simulation from the model, then computes distance of output from a
+    * target data set
+    * 
+    * @return The collection of generated parameters together with their
+    * simulated distances from a target data set
+    */
   def run[P](n: Int, rprior: => P, dist: P => Double): GenSeq[(P, Double)] = {
     (1 to n).par.map(i => {
       val p = rprior
@@ -24,7 +38,14 @@ object Abc {
     })
   }
 
-  // TODO: scaladoc
+  /**
+    * Generate some basic diagnostics associated with an ABC run.
+    * Called purely for the side effect of generating output on the
+    * console. 
+    * 
+    * @param output Collection of runs (and distances) such as generated
+    * from `run`
+    */
   def summary[P: CsvRow](output: GenSeq[(P, Double)]): Unit = {
     Mcmc.summary(Mcmc.toDMD(output map (_._1)))
   }
