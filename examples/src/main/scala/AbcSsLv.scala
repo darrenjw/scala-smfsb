d439b45493c7376efcd10315645f3fa857990f87
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-11-26 22:55:42 +0000

    started on summary stats example

--- examples/src/main/scala/AbcSsLv.scala
+++ examples/src/main/scala/AbcSsLv.scala
@@ -1,7 +1,7 @@
 /*
-AbcLv.scala
+AbcSsLv.scala
 
-Inference for the parameters of a Lotka-Volterra model using ABC
+Inference for the parameters of a Lotka-Volterra model using ABC with summary statistics
 
  */
 
@@ -16,18 +16,26 @@ object AbcSsLv {
   // state prior
   def statePriorSample = DenseVector(Poisson(50.0).draw, Poisson(100.0).draw)
   def rprior = exp(DenseVector(Uniform(-3.0,3.0).draw,Uniform(-8.0,-2.0).draw,Uniform(-4.0,2.0).draw))
-  def distance(real: Ts[DoubleState])(sim: Ts[IntState]): Double =
-    math.sqrt((real zip sim).map{case (r,s) => r._2(0)-s._2(0)}.map(x=>x*x).reduce(_+_))
+  def distance(real: DoubleState)(sim: DoubleState): Double =
+    math.sqrt(sum((real-sim).map(x => x*x)))
+  def ssd(ts: Ts[DoubleState]): DoubleState = {
+    val v = DenseVector(ts.map(_._2(0)).toArray)
+    v // TODO: real summary stats!!!
+  }
+  def ss(ts: Ts[IntState]): DoubleState = {
+    ssd(ts.map{case (t,v) => (t,v.map(_.toDouble))})
+  }
+  // TODO: Calibrate summary stats
   def step(p: DoubleState) = Step.gillespie(SpnModels.lv[IntState](p), maxH=1e5)
 
   def main(args: Array[String]): Unit = {
-    println("ABC rejection demo...")
-    val n = 100000 // required number of iterations from the ABC algorithm
+    println("ABC rejection demo (with summary stats)...")
+    val n = 10000 // required number of iterations from the ABC algorithm
     val fraction = 0.01 // fraction of accepted ABC samples
     val rawData = Source.fromFile("LVpreyNoise10.txt").getLines
     val data = ((0 to 30 by 2).toList zip rawData.toList).map((x: (Int,String)) => (x._1.toDouble, DenseVector(x._2.toDouble)))
-    val dist = distance(data) _
-    def rdist(p: DoubleState): Double = dist(Sim.ts[IntState](statePriorSample,0.0,30.0,2.0,step(p)))
+    val dist = distance(ssd(data)) _
+    def rdist(p: DoubleState): Double = dist(ss(Sim.ts[IntState](statePriorSample,0.0,30.0,2.0,step(p))))
     println("Starting ABC run now...")
     val out = Abc.run(n,rprior,rdist _)
     println("ABC run completed.")
