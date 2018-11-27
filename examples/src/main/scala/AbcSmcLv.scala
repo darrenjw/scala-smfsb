fea444d58dfec97947b620663cc2be3cb501d347
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-11-27 14:43:05 +0000

    use Ss as template for Smc

--- examples/src/main/scala/AbcSmcLv.scala
+++ examples/src/main/scala/AbcSmcLv.scala
@@ -1,7 +1,7 @@
 /*
-AbcLv.scala
+AbcSsLv.scala
 
-Inference for the parameters of a Lotka-Volterra model using ABC
+Inference for the parameters of a Lotka-Volterra model using ABC with summary statistics
 
  */
 
@@ -9,49 +9,58 @@ import smfsb._
 import scala.io.Source
 import breeze.linalg._
 import breeze.numerics._
+import breeze.stats._
 import breeze.stats.distributions._
 
 object AbcSmcLv {
 
-  // state prior
   def statePriorSample = DenseVector(Poisson(50.0).draw, Poisson(100.0).draw)
-  // def statePrior(n: Int) = (1 to n).map(i => statePriorSample).toVector
+
   def rprior = exp(DenseVector(Uniform(-3.0,3.0).draw,Uniform(-8.0,-2.0).draw,Uniform(-4.0,2.0).draw))
-  // MCMC proposal
-  def rprop(p: DoubleState, tune: Double = 0.01): DoubleState = p*exp(DenseVector(Gaussian(0.0,0.01).sample(3).toArray)) 
-  def dprop(n: DoubleState, o: DoubleState): Double = 1.0 // Not really true but OK here...
-  // data log-likelihood
-  // def obsLik(th: DoubleState)(s: IntState, o: DoubleState): LogLik =
-  //  Gaussian(s.data(0).toDouble, 10.0).logPdf(o(0))
-  def distance(real: Ts[DoubleState])(sim: Ts[IntState]): Double =
-    math.sqrt((real zip sim).map{case (r,s) => r._2(0)-s._2(0)}.map(x=>x*x).reduce(_+_))
-  def step(p: DoubleState) = Step.gillespie(SpnModels.lv[IntState](p))
+
+  def distance(real: DoubleState)(sim: DoubleState): Double =
+    math.sqrt(sum((real-sim).map(x => x*x)))
+
+  def ssds(ts: Ts[DoubleState]): DoubleState = {
+    val v = DenseVector(ts.map(_._2(0)).toArray)
+    import breeze.stats._
+    val m = meanAndVariance(v)
+    val acf = Mcmc.acf(v.data, 3)
+    DenseVector(m.mean, math.sqrt(m.variance), acf(1), acf(2), acf(3))
+  }
+
+  def ss(ts: Ts[IntState]): DoubleState = {
+    ssds(ts.map{case (t,v) => (t,v.map(_.toDouble))})
+  }
+
+  def step(p: DoubleState) = Step.gillespie(SpnModels.lv[IntState](p), maxH=1e5)
 
   def main(args: Array[String]): Unit = {
-    println("ABC rejection demo...")
-    //val N = 100 // number of particles in the particle filter
+    println("ABC rejection demo (with summary stats)...")
     val n = 100000 // required number of iterations from the ABC algorithm
     val fraction = 0.01 // fraction of accepted ABC samples
-    //val thin = 5 // thinning
-    //val burn = 10 // initial discarded MCMC iterations
-    //val tune = 0.01 // tuning parameter of the MH proposal
-    // first read in the observational data
     val rawData = Source.fromFile("LVpreyNoise10.txt").getLines
     val data = ((0 to 30 by 2).toList zip rawData.toList).map((x: (Int,String)) => (x._1.toDouble, DenseVector(x._2.toDouble)))
-    val dist = distance(data) _
-    def rdist(p: DoubleState): Double = dist(Sim.ts[IntState](statePriorSample,0.0,30.0,2.0,step(p)))
-    //println(data)
-    //Sim.plotTs(data)
-    println("Starting ABC run now...")
+    def ss1(p: DoubleState): DoubleState = ss(Sim.ts[IntState](statePriorSample,0.0,30.0,2.0,step(p)))
+    println("Starting ABC pilot run...")
+    val out1 = Abc.run(1500,rprior,ss1 _)
+    println("Pilot run completed.")
+    val outMat = Mcmc.toDMD(out1 map (_._2))
+    val sds = sqrt(variance(outMat(::, *))).t
+    println(sds)
+    def ss2(ts: Ts[IntState]): DoubleState = ss(ts) /:/ sds
+    val ssd = ssds(data) /:/ sds
+    val dist = distance(ssd) _
+    def rdist(p: DoubleState): Double = dist(ss2(Sim.ts[IntState](statePriorSample,0.0,30.0,2.0,step(p))))
+    println("Starting main ABC run...")
     val out = Abc.run(n,rprior,rdist _)
-    println("ABC run completed.")
+    println("Main ABC run completed.")
     val distances = (out map (_._2)).seq
     import breeze.stats.DescriptiveStats._
     val cutoff = percentile(distances, fraction)
     val accepted = out filter (_._2 < cutoff)
-    //println(accepted.length)
-    //Abc.summary(out)(dvdState)
-    Abc.summary(accepted)(dvdState)
+    val laccepted = accepted map (x => log(x._1))
+    Mcmc.summary(laccepted,true)
     println("Done.")
   }
 
