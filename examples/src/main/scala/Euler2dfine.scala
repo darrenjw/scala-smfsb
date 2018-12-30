a897fa5c5226057cb47e16a01deb9135063f5841
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-12-30 14:26:12 +0000

    minor edits to animated reaction-diffusion examples

--- examples/src/main/scala/Euler2dfine.scala
+++ examples/src/main/scala/Euler2dfine.scala
@@ -1,8 +1,8 @@
 /*
-Cle2dfine.scala
+Euler2dfine.scala
 
-Approximate simulation from a 2d reaction-diffusion master equation model using the CLE on a fine grid
-Essentially numerically integrating a SPDE
+Continuous deterministic Euler simulation from a 2d reaction-diffusion system
+Essentially numerically integrating a PDE
 
  */
 
@@ -11,28 +11,28 @@ import breeze.linalg.{Vector => BVec, _}
 import breeze.numerics._
 import breeze.plot._
 
-object Cle2dfine {
+object Euler2dfine {
 
   def main(args: Array[String]): Unit = {
-    println("2d approximate spatial CLE simulation from a reaction-diffusion Lotka-Volterra model - will run FOREVER - just kill it when you get bored!")
-    val r = 80
-    val c = 100
+    println("2d reaction-diffusion PDE for the Lotka-Volterra model - will run FOREVER - just kill it when you get bored!")
+    val r = 400
+    val c = 500
     val model = SpnModels.lv[DoubleState]()
-    val step = Spatial.cle2d(model, DenseVector(0.5, 0.5), 0.1)
+    val step = Spatial.euler2d(model, DenseVector(0.5, 0.5), 0.05)
     val x00 = DenseVector(0.0, 0.0)
     val x0 = DenseVector(50.0, 100.0)
     val xx00 = PMatrix(r, c, Vector.fill(r*c)(x00))
     val xx0 = xx00.updated(c/2, r/2, x0)
 
     // create an infinite stream of states
-    val s = Stream.iterate(xx0)(x => step(x, 0.0, 0.5))
+    val s = Stream.iterate(xx0)(x => step(x, 0.0, 2.0))
 
     // animate computation of the stream
     val f = Figure("2d spatial CLE simulation of the LV model")
     s.foreach(x => {
       f.clear
-      f.width = 500
-      f.height = 800
+      f.width = 700
+      f.height = 1000
       val p0 = f.subplot(2, 1, 0)
       p0 += image(PMatrix.toBDM(x map (_.data(0))))
       p0.title = "Prey"
@@ -40,6 +40,7 @@ object Cle2dfine {
       p1 += image(PMatrix.toBDM(x map (_.data(1))))
       p1.title = "Predator"
       f.refresh
+      Thread.sleep(2000) // slow it down!
     })
 
     println("Finished")
