80c1b19b0d9f3f9679274a99b73b53862ec48fe4
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-12-30 12:54:12 +0000

    working 2d CLE animation on a fine grid

--- examples/src/main/scala/Cle2dfine.scala
+++ examples/src/main/scala/Cle2dfine.scala
@@ -13,22 +13,33 @@ import breeze.plot._
 object Cle2dfine {
 
   def main(args: Array[String]): Unit = {
-    println("2d approximate CLE simulation from an RDME model - will take a while...")
-    val r = 200
-    val c = 250
+    println("2d approximate spatial CLE simulation from a reaction-diffuion Lotka-Volterra model - will run FOREVER - just kill it when you get bored!")
+    val r = 80
+    val c = 100
     val model = SpnModels.lv[DoubleState]()
-    val step = Spatial.cle2d(model, DenseVector(0.6, 0.6), 0.05)
+    val step = Spatial.cle2d(model, DenseVector(0.5, 0.5), 0.05)
     val x00 = DenseVector(0.0, 0.0)
     val x0 = DenseVector(50.0, 100.0)
     val xx00 = PMatrix(r, c, Vector.fill(r*c)(x00))
     val xx0 = xx00.updated(c/2, r/2, x0)
-    val output = step(xx0, 0.0, 3.0)
-    println(output)
-    val f = Figure("LV")
-    val p0 = f.subplot(2, 1, 0)
-    p0 += image(PMatrix.toBDM(output map (_.data(0))))
-    val p1 = f.subplot(2, 1, 1)
-    p1 += image(PMatrix.toBDM(output map (_.data(1))))
+
+    // create an infinite stream of states
+    val s = Stream.iterate(xx0)(x => step(x,0.0,0.2))
+
+    // animate computation of the stream
+    val f = Figure("2d spatial CLE simulation of the LV model")
+    s.foreach(x => {
+      f.clear
+      f.width = 500
+      f.height = 800
+      val p0 = f.subplot(2, 1, 0)
+      p0 += image(PMatrix.toBDM(x map (_.data(0))))
+      p0.title = "Prey"
+      val p1 = f.subplot(2, 1, 1)
+      p1 += image(PMatrix.toBDM(x map (_.data(1))))
+      p1.title = "Predator"
+    })
+
     println("Finished")
   }
 
