5dbcfd0b87b970be10636e837f1e623eb57328df
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-01-04 18:50:13 +0000

    updated Cle example

--- examples/src/main/scala/Cle2dfine.scala
+++ examples/src/main/scala/Cle2dfine.scala
@@ -15,17 +15,17 @@ object Cle2dfine {
 
   def main(args: Array[String]): Unit = {
     println("2d approximate spatial CLE simulation from a reaction-diffusion Lotka-Volterra model - will run FOREVER - just kill it when you get bored!")
-    val r = 80
-    val c = 100
+    val r = 300
+    val c = 350
     val model = SpnModels.lv[DoubleState]()
-    val step = Spatial.cle2d(model, DenseVector(0.5, 0.5), 0.1)
+    val step = Spatial.cle2d(model, DenseVector(0.5, 0.5), 0.05)
     val x00 = DenseVector(0.0, 0.0)
     val x0 = DenseVector(50.0, 100.0)
     val xx00 = PMatrix(r, c, Vector.fill(r*c)(x00))
     val xx0 = xx00.updated(c/2, r/2, x0)
 
     // create an infinite stream of states
-    val s = Stream.iterate(xx0)(x => step(x, 0.0, 0.5))
+    val s = Stream.iterate(xx0)(x => step(x, 0.0, 1.0))
 
     // animate computation of the stream
     val f = Figure("2d spatial CLE simulation of the LV model")
