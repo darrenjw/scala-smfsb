4cab33b2f4bc34e70e3ebb75ebec6374b6413c43
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2020-12-23 22:22:01 +0000

    remove old tests

--- examples/src/test/scala/examples-test.scala
+++ examples/src/test/scala/examples-test.scala
@@ -8,56 +8,15 @@ Some basic tests
 package smfsb
 
 import org.scalatest._
-import org.scalatest.junit._
-// import org.scalatest.prop._
-import org.junit.runner.RunWith
+import funsuite._
 
 import breeze.linalg._
-import Types._
+import breeze.stats.distributions._
 
-@RunWith(classOf[JUnitRunner])
-class MyTestSuite extends FunSuite {
+class MyTestSuite extends AnyFunSuite {
 
-
-  test("pfMll creation and evaluation") {
-    import LvPmmh._
-    import SpnExamples._
-    import Mll._
-    import scala.io.Source
-    val rawData = Source.fromFile("../LVpreyNoise10.txt").getLines
-    val data = ((0 to 30 by 2).toList zip rawData.toList) map { x => (x._1.toDouble, DenseVector(x._2.toDouble)) }
-    val mll = pfMll(160, simPrior, 0.0, stepLv, obsLik, data)
-    val mlle = mll(lvparam)
-    // println(mlle)
-    assert(mlle < 0.0)
-  }
-
-  test("Parallel pfMll creation and evaluation") {
-    import LvPmmh._
-    import SpnExamples._
-    import Mll._
-    import scala.io.Source
-    val rawData = Source.fromFile("../LVpreyNoise10.txt").getLines
-    val data = ((0 to 30 by 2).toList zip rawData.toList) map { x => (x._1.toDouble, DenseVector(x._2.toDouble)) }
-    val mll = pfMllP(160, simPrior, 0.0, stepLv, obsLik, data)
-    val mlle = mll(lvparam)
-    // println(mlle)
-    assert(mlle < 0.0)
-  }
-
-  test("Serial and parallel pfMll should be similar") {
-    import LvPmmh._
-    import SpnExamples._
-    import Mll._
-    import scala.io.Source
-    val rawData = Source.fromFile("../LVpreyNoise10.txt").getLines
-    val data = ((0 to 30 by 2).toList zip rawData.toList) map { x => (x._1.toDouble, DenseVector(x._2.toDouble)) }
-    val mll = pfMll(320, simPrior, 0.0, stepLv, obsLik, data)
-    val mllp = pfMllP(320, simPrior, 0.0, stepLv, obsLik, data)
-    val mlle = mll(lvparam)
-    val mllep = mllp(lvparam)
-    //println(mlle+" "+mllep)
-    assert(math.abs(mlle-mllep) < 2.0)
+  test("dummy test") {
+    assert(1 === 1)
   }
 
 
